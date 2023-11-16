8b29604cd4bb2b32d50eba3f15ddccdf2a19ec1a
Cadence James <88948625+cadencejames@users.noreply.github.com>
2023-11-16 14:17:48 -0800

    Create csvdata.js
    initial commit

---
+++ csvdata.js
@@ -0,0 +1,20 @@
+//The Raw CSV data as a string
+const csvData = `
+site,tier,avg,01-Nov-23,02-Nov-23,03-Nov-23
+SiteA,1,5,5,5,5
+SiteA,1,5,5,5,5
+SiteB,1,6,6,6,6
+SiteB,1,6,6,6,6
+SiteC,1,1,1,1,1
+SiteC,1,1,1,1,1
+SiteD,2,2,2,2,2
+SiteD,2,2,2,2,2
+SiteE,3,10,5,5,5
+SiteE,3,10,5,5,5
+SiteF,3,6,x,x,6
+SiteF,3,6,x,x,6
+SiteG,3,32,1,1,1
+SiteG,3,32,1,1,1
+SiteH,3,24,2,24,2
+SiteH,3,24,2,24,2
+`;
